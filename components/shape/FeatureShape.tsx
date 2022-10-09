a0d6c53187730215effece9cd8b443ac619ff68c
Semet <hamdanilombok@gmail.com>
2022-10-09 16:31:38 +0800

    add FeatureShape component

---
+++ components/shape/FeatureShape.tsx
@@ -0,0 +1,35 @@
+import { Box } from "@chakra-ui/react";
+import React from "react";
+
+const FeatureShape = () => {
+	return (
+		<Box
+			position={"absolute"}
+			bottom={"-79px"}
+			left={0}
+			w={"full"}
+			overflow={"hidden"}
+			lineHeight={0}
+		>
+			<svg
+				data-name="Layer 1"
+				xmlns="http://www.w3.org/2000/svg"
+				viewBox="0 0 1200 120"
+				preserveAspectRatio="none"
+				style={{
+					position: "relative",
+					display: "block",
+					width: "calc(100% + 1.3px)",
+					height: "81px",
+				}}
+			>
+				<path
+					d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
+					style={{ fill: "#F7FAFC" }}
+				></path>
+			</svg>
+		</Box>
+	);
+};
+
+export default FeatureShape;
